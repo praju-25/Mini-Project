72ec2dad520ca1f0f9e981524e132468ddde13be
Prajakta <shindeprajakta1425@gmail.com>
2023-06-21 14:48:49 +0530

    Project  added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { LocalizedString } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login",component:LoginComponent},
+  {path:"registration",component:RegistrationComponent},
+  {path:"",redirectTo:"/login",pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
